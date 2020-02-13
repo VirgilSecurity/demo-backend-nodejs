0d7d90903532b41ea6237d91334e07f69a86263f
cardoso <matheus@cardo.so>
2020-02-13 12:25:55 -0300

    Update to NodeJS 12, Virgil Crypto & Virgil SDK Alpha

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
-
-const { requireAuthHeader } = require('./api/userValidation');
-const { authenticate } = require('./api/authenticate');
-const { generateVirgilJwt } = require('./api/virgilToken');
+const { initCrypto } = require('virgil-crypto');
 
 const app = express();
 
-app.use(cors({ origin: true, methods: 'OPTIONS,POST,GET', }));
-app.use(express.json());
+initCrypto().then(() => {
+    const { requireAuthHeader } = require('./api/userValidation');
+    const { authenticate } = require('./api/authenticate');
+    const { generateVirgilJwt } = require('./api/virgilToken');
 
-app.post('/authenticate', authenticate);
-app.get('/virgil-jwt', requireAuthHeader, generateVirgilJwt);
-app.use(express.static('./public/'));
+    app.use(cors({ origin: true, methods: 'OPTIONS,POST,GET', }));
+    app.use(express.json());
+
+    app.post('/authenticate', authenticate);
+    app.get('/virgil-jwt', requireAuthHeader, generateVirgilJwt);
+    app.use(express.static('./public/'));
+});
 
 module.exports = app;
